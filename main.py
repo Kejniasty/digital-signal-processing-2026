fb38ae40d43ae500d3164993cf1415afdc37aa93
kejniasty <kejniasty@gmail.com>
2026-04-03 13:39:39 +0200

    added discrete plot generation

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ if __name__ == "__main__":
     test_signal = signal.generate_continuous_signal(10, 5.5, 0, 1,
                                                     signal.SignalType.TRIANGULAR, 0.6)
 
+    test_discrete_signal = signal.generate_discrete_signal(10, 5.5, 0, 1,
+                                                           signal.SignalType.IMPULSE_NOISE, 0.1)
     print(test_signal)
     plot.generate_plot(test_signal)
+    plot.generate_discrete_plot(test_discrete_signal)
--- plot.py
+++ plot.py
@@ -14,4 +14,18 @@ def generate_plot(signal: "Signal") -> None:
 
     plt.show()
 
+def generate_discrete_plot(signal: "Signal") -> None:
+    t = np.arange(signal.start_time, signal.duration + signal.start_time, 1 / signal.sample_rate)
+    s = signal.signal.copy()
+
+    plt.figure(figsize=(10, 4))
+    markerline, stemlines, baseline = plt.stem(t, signal.signal, linefmt='b-', markerfmt='bo', basefmt='r-')
+
+    plt.setp(stemlines, 'linewidth', 0.1)
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
+    plt.title('Discrete Signal Plot')
+    plt.grid(True, linestyle='--', alpha=0.7)
+
+    plt.show()
 
